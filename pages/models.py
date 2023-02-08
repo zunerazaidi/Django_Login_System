66a3be2ced9a5c3626d3f32ae45a3f5f0f83ffc3
zunerazaidi <zunera.zaidi@ucalgary.ca>
2023-02-07 20:17:04 -0800

    Updating html files

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+class Member(models.Model):
+    fname = models.CharField(max_length=20)
+    lname = models.CharField(max_length=20)
+    email = models.EmailField(max_length=100)
+    telephone = models.CharField(max_length=50)
+    role = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
 
-# Create your models here.
+    def __str__(self):
+        return self.fname + ' ' + self.lname
--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Member
+
 
 
 def login_page(request):
     return render(request, 'login_page.html', {})
 def team_member_list(request):
-    return render(request, 'team_member_list.html', {})
+    all_members = Member.objects.all
+    member_count = Member.objects.all().count()
+    return render(request, 'team_member_list.html', {'all_members':all_members, 'member_count': member_count})
 
 def add_team_member(request):
    return render(request, 'add_team_member.html', {})
